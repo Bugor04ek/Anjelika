21ae4620e769db98fff1e18faf70d2ffd56bf9e7
Bugor <andreyvlasov03@ya,ru>
2023-10-22 20:41:19 +0300

    Формируется таблица с вычисляемым полем "Длина куска 1 корзины". В екселе от форматированы столбцы под числа. значения 'nan' теперь 0. Добавлена константа d_mult = 2.08

--- CreateTable.py
+++ CreateTable.py
@@ -6,7 +6,12 @@ file_name = 'Заказы.xlsx'
 
 
 def merge_table(table1, table2):
-
+    """
+        Объединяет 2 таблицы с одинаковым количеством строк
+    :param table1: основная таблица со всеми параметрами
+    :param table2: таблица с длинами кусков в корзине
+    :return: объединенная таблица
+    """
     temp_table = PrettyTable()
     temp_table_rows = []
     counter = 0
@@ -19,23 +24,67 @@ def merge_table(table1, table2):
     field.extend(table1.field_names)
     field.extend(table2.field_names)
 
-    temp_table_rows.field_names = field
-    temp_table_rows.add_rows(temp_table_rows)
+    temp_table.field_names = field
+    temp_table.add_rows(temp_table_rows)
 
-def calculating_length_piece(table):
+    return temp_table
 
-    for row in table.rows:
-        a = row
 
-# 1 - Номер счета
-# 2 - Номенклатура ERP
-# 3 - Дата выпуска по заказу
-# 4 - Диаметр проволоки (волочение), мм
-# 5 - Диаметр проволоки на волочении
-# 6 - Количество километров в производство
+def calculating_length_piece(table, table_param):
+    """
+        Процедура параметры из table_param и вычисляет длину меди потраченной с каждой корзины на мультике. Все
+        вычисленные длины записываются в таблицу result и передается в функцию merge_table
+    :param table: основная таблица со всеми параметрами
+    :param table_param: таблица для вычислений длин куска
+    :return: возвращает таблицу с дополнительным столбцом полученная из функции merge_table
+    """
+    result = PrettyTable(['Длина куска 1 корзины'])
 
-# excel_data = pd.read_excel(file_name, usecols=[1, 2, 3, 4, 5, 6])
-# data = pd.DataFrame(excel_data)
+    for row in table_param.rows:
+        order_length = row[0]
+        count_veins = int(row[1])
+        count_strands = int(row[2])
+        delays_vein = int(row[3])
+        diameter = row[4]
+
+        # суммарная длина проволочек
+        total_length_delays = delays_vein * count_strands * count_veins * order_length
+
+        length_piece = total_length_delays * (diameter ** 2 / d_mult ** 2)
+
+        if str(row[5]) != 'nan':
+            count_veins_plus = int(row[5])
+            count_strands_plus = row[6]
+            delays_vein_plus = row[7]
+            diameter_plus = row[8]
+
+            # суммарная длина проволочек плюсовой части
+            total_length_delays_plus = delays_vein_plus * count_strands_plus * count_veins_plus * order_length
+
+            length_piece_plus = total_length_delays_plus * (diameter_plus ** 2 / d_mult ** 2)
+        else:
+            length_piece_plus = 0
+
+        length_piece = round(length_piece + length_piece_plus, 3)
+
+        result.add_row([length_piece])
+
+    return merge_table(table, result)
+
+
+# B - Номер счета
+# C - Номенклатура ERP
+# D - Дата выпуска по заказу
+# E - Количество километров в производство
+# F - Диаметр проволоки (волочение), мм
+# J - На волочение, мм
+# N - Диаметр проволоки (волочение), мм
+
+
+excel_data = pd.read_excel(file_name, usecols="B:F, J, N")
+data = pd.DataFrame(excel_data).fillna(0)
+for row in data.values:
+    first_param_table.add_row(row)
 
 # E - Количество километров в производство
 # G  - Количество жил
@@ -52,5 +101,6 @@ data = pd.DataFrame(excel_data_calculation)
 for row in data.values:
     second_param_table.add_row(row)
 
-calculating_length_piece(second_param_table)
-print("The content of the file is:\n", second_param_table)
+result_table = calculating_length_piece(first_param_table, second_param_table)
+
+print("The content of the file is:\n", result_table)
