e4a1adec8394c9af2fc7d4ef02bfd1c8abb4ca3e
Bugor <andreyvlasov03@ya.ru>
2023-10-23 14:27:23 +0300

    файл теперь нельзя запустить, все функции вызываются из main. Файл состоит из 2 связанных функций для работы с таблицами. merge_table - потеряла актуальность, я научился добавлять столбцы через add_column

--- CreateTable.py
+++ CreateTable.py
@@ -5,41 +5,16 @@ import numpy as np
 file_name = 'Заказы.xlsx'
 
 
-def merge_table(table1, table2):
-    """
-        Объединяет 2 таблицы с одинаковым количеством строк
-    :param table1: основная таблица со всеми параметрами
-    :param table2: таблица с длинами кусков в корзине
-    :return: объединенная таблица
-    """
-    temp_table = PrettyTable()
-    temp_table_rows = []
-    counter = 0
-    for i in table1.rows:
-        i.extend(table2.rows[counter])
-        counter += 1
-        temp_table_rows.append(i)
-
-    field = []
-    field.extend(table1.field_names)
-    field.extend(table2.field_names)
-
-    temp_table.field_names = field
-    temp_table.add_rows(temp_table_rows)
-
-    return temp_table
-
-
 def calculating_length_piece(table, table_param):
     """
-        Процедура параметры из table_param и вычисляет длину меди потраченной с каждой корзины на мультике. Все
-        вычисленные длины записываются в таблицу result и передается в функцию merge_table
+    Процедура параметры из table_param и вычисляет длину меди потраченной с каждой корзины на мультике. Все
+    вычисленные длины записываются во временный массив, а потом добавляются столбцом в table
     :param table: основная таблица со всеми параметрами
     :param table_param: таблица для вычислений длин куска
-    :return: возвращает таблицу с дополнительным столбцом полученная из функции merge_table
     """
-    result = PrettyTable(['Длина куска 1 корзины'])
 
+    # временный массив для длины кусков
+    res = []
     for row in table_param.rows:
         order_length = row[0]
         count_veins = int(row[1])
@@ -52,7 +27,7 @@ def calculating_length_piece(table, table_param):
 
         length_piece = total_length_delays * (diameter ** 2 / d_mult ** 2)
 
-        if str(row[5]) != 'nan':
+        if row[5] != '0':
             count_veins_plus = int(row[5])
             count_strands_plus = row[6]
             delays_vein_plus = row[7]
@@ -67,40 +42,46 @@ def calculating_length_piece(table, table_param):
 
         length_piece = round(length_piece + length_piece_plus, 3)
 
-        result.add_row([length_piece])
+        res.append(length_piece)
 
-    return merge_table(table, result)
-
-
-# B - Номер счета
-# C - Номенклатура ERP
-# D - Дата выпуска по заказу
-# E - Количество километров в производство
-# F - Диаметр проволоки (волочение), мм
-# J - На волочение, мм
-# N - Диаметр проволоки (волочение), мм
+    table.add_column('Длина куска 1 корзины', res)
 
 
-excel_data = pd.read_excel(file_name, usecols="B:F, J, N")
-data = pd.DataFrame(excel_data).fillna(0)
-for row in data.values:
-    first_param_table.add_row(row)
+def create_tables():
+    """
+        Создание 2 таблиц:
+        1. Основная, содержащая информацию о заказе
+        2. Таблица с параметрами для вычисления "Длина куска 1 корзины"
+    :return: result_table -- результирующая таблица с вычисленным столбцом "Длина куска 1 корзины"
+    """
+    # B - Номер счета
+    # C - Номенклатура ERP
+    # D - Дата выпуска по заказу
+    # E - Количество километров в производство
+    # F - Диаметр проволоки (волочение), мм
+    # J - На волочение, мм
+    # N - Диаметр проволоки (волочение), мм
 
-# E - Количество километров в производство
-# G  - Количество жил
-# H  - Кол-во стренг
-# I  - Всего проволочек в 1 стренге
-# J - Диаметр проволоки на волочении
-# K - Количество жил плюсовой
-# L - Кол-во стренг плюсовой
-# M - Всего проволочек в1 стренге плюсовой
-# N - Диаметр проволоки (волочение), мм
+    excel_data = pd.read_excel(file_name, usecols="B:F, J, N")
+    data = pd.DataFrame(excel_data).fillna(0)
+    for row in data.values:
+        first_param_table.add_row(row)
 
-excel_data_calculation = pd.read_excel(file_name, usecols="E,G:N")
-data = pd.DataFrame(excel_data_calculation)
-for row in data.values:
-    second_param_table.add_row(row)
+    # E - Количество километров в производство
+    # G  - Количество жил
+    # H  - Кол-во стренг
+    # I  - Всего проволочек в 1 стренге
+    # J - Диаметр проволоки на волочении
+    # K - Количество жил плюсовой
+    # L - Кол-во стренг плюсовой
+    # M - Всего проволочек в1 стренге плюсовой
+    # N - Диаметр проволоки (волочение), мм
 
-result_table = calculating_length_piece(first_param_table, second_param_table)
+    excel_data_calculation = pd.read_excel(file_name, usecols="E,G:N")
+    data = pd.DataFrame(excel_data_calculation).fillna(0)
+    for row in data.values:
+        second_param_table.add_row(row)
 
-print("The content of the file is:\n", result_table)
+    calculating_length_piece(first_param_table, second_param_table)
+
+    return first_param_table
