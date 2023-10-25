195b73727fd7aa41104c9398c60240a7501d6ab9
Bugor <andreyvlasov03@ya.ru>
2023-10-25 18:21:03 +0300

    изменил структуру таблицы, названия переменных и файл экселя

--- CreateTable.py
+++ CreateTable.py
@@ -21,14 +21,28 @@ def calculating_length_piece(table, table_param):
         # вместимостью 32 км, дает один полный барабан и один, вместимостью 8 км, но если прядей 4, то будет 4 полных
         # барабана, и 4 вместимостью 8 км).
 
+        # 0 - Кол-во километров в производство
+        # 1 - Кол-во жил
+        # 2 - Перед мультиком -> d_mult = 2.08
+        # 3 - Диаметр проволоки
+        # 4 - Кол-во стренг
+        # 5 - Кол-во прядей
+        # 6 - Кол-во проволок в пряди
+        # 7 - Кол-во прядей доп
+        # 8 - Кол-во проволок доп
+
         order_length = row[0]
-        count_veins = int(row[1])
-        count_strands = int(row[2])
-        delays_vein = int(row[3])
-        diameter = row[4]
+        number_of_veins = int(row[1])
+        number_of_strands = int(row[4])
+        number_of_sliver = int(row[5])
+        wires_in_sliver = int(row[6])
+        number_of_sliver_extra = int(row[7])
+        wires_in_sliver_extra = int(row[8])
+        diameter = row[3]
 
         # суммарная длина проволочек
-        total_length_delays = delays_vein * count_strands * count_veins * order_length
+        total_length_delays = ((number_of_sliver * wires_in_sliver + number_of_sliver_extra * wires_in_sliver_extra)
+                               * number_of_strands * number_of_veins * order_length)
 
         length_piece = total_length_delays * (diameter ** 2 / d_mult ** 2)
 
@@ -54,39 +68,45 @@ def calculating_length_piece(table, table_param):
 
 def create_tables():
     """
-        Создание 2 таблиц:
-        1. Основная, содержащая информацию о заказе
-        2. Таблица с параметрами для вычисления "Длина куска 1 корзины"
+    Создание 2 таблиц:
+    1. Основная, содержащая информацию о заказе
+    2. Таблица с параметрами для вычисления "Длина куска 1 корзины"
     :return: result_table -- результирующая таблица с вычисленным столбцом "Длина куска 1 корзины"
     """
+    # A - IDZak
     # B - Номер счета
-    # C - Номенклатура ERP
+    # C - Марка
     # D - Дата выпуска по заказу
     # E - Количество километров в производство
-    # F - Диаметр проволоки (волочение), мм
-    # J - На волочение, мм
-    # N - Диаметр проволоки (волочение), мм
+    # F - Количество жил
+    # G - Перед мультиком -> d_mult = 2.08
+    # H - Диаметр проволоки
+    # N - Вид барабана
+    # O - Количество заправок
+    # P - Километраж масса VS Длина
 
-    excel_data = pd.read_excel(file_name, usecols="B:F, J, N")
+    excel_data = pd.read_excel(file_name, usecols="A:H, N:P")
     data = pd.DataFrame(excel_data).fillna(0)
     for row in data.values:
-        first_param_table.add_row(row)
+        main_table.add_row(row)
 
     # E - Количество километров в производство
-    # G  - Количество жил
-    # H  - Кол-во стренг
-    # I  - Всего проволочек в 1 стренге
-    # J - Диаметр проволоки на волочении
-    # K - Количество жил плюсовой
-    # L - Кол-во стренг плюсовой
-    # M - Всего проволочек в1 стренге плюсовой
-    # N - Диаметр проволоки (волочение), мм
+    # F - Количество жил
+    # G - Перед мультиком -> d_mult = 2.08
+    # H - Диаметр проволоки
+    # I - Количество стренг
+    # J - Кол-во прядей
+    # K - Кол-во проволок в пряди
+    # L - Кол-во прядей доп
+    # M - Кол-во проволок доп
 
-    excel_data_calculation = pd.read_excel(file_name, usecols="E,G:N")
+    print(main_table)
+
+    excel_data_calculation = pd.read_excel(file_name, usecols="E, F, H:M")
     data = pd.DataFrame(excel_data_calculation).fillna(0)
     for row in data.values:
         second_param_table.add_row(row)
 
-    calculating_length_piece(first_param_table, second_param_table)
+    calculating_length_piece(main_table, second_param_table)
 
-    return first_param_table
+    #return first_param_table
